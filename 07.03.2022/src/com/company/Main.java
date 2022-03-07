19a96e5b8ae52685765d8449d26c6fdc6a4d8af4
BurakKontas <aburakkontas@trakya.edu.tr>
2022-03-07 16:00:13 +0300

    C stili kuyruk ama array erroru var

--- 07.03.2022/src/com/company/Main.java
+++ 07.03.2022/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
 public class Main {
+    static int elemanSayisi = 10;
+    static int isaretci = 0;
+    static int[] kuyruk = new int[elemanSayisi];
+
+    static void ekle(int eklenecek) {
+        kuyruk[isaretci]=eklenecek;
+        isaretci++;
+    }
+
+    static int sil() {
+        int deger = kuyruk[0];
+        for (int i = 0; i < isaretci; i++) kuyruk[i-1] = kuyruk[i];
+        isaretci--;
+        return deger;
+    }
+
+    static String listele() {
+        String cikti = "";
+        for(int i=0;i<isaretci;i++) cikti += kuyruk[i]+"<-";
+        return cikti;
+    }
 
     public static void main(String[] args) {
 	// write your code here
+    ekle(5);
+    System.out.println(listele());
+    ekle(2);
+    System.out.println(listele());
+    System.out.println("Çıkartılan Eleman "+sil());
+    System.out.println(listele());
     }
 }
