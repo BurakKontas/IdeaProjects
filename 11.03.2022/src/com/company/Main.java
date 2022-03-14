7b2287698a56611e0aa77fc16e0fc5b8d4acdfc7
BurakKontas <aburakkontas@trakya.edu.tr>
2022-03-14 15:34:23 +0300

    Temel Fonksiyonlar

--- 11.03.2022/src/com/company/Main.java
+++ 11.03.2022/src/com/company/Main.java
@@ -1,13 +1,35 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+class Rand {
+    private List<Integer> liste;
+    Random random;
+
+    public Rand(){
+        liste = new ArrayList<>();
+        random = new Random();
+    }
+
+    public List<Integer> intListesiAl(int elemanSayisi) {
+        for (int i = 0; i < elemanSayisi; i++)
+            liste.add(random.nextInt());
+        return liste;
+    }
+}
 
 public class Main {
 
+    static void listele(List<Integer> liste) {
+        for(var eleman:liste) System.out.println(eleman);
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        Random random = new Random();
-        var deger = random.nextInt();
-        System.out.println(deger);
+        var r = new Rand();
+        var sonuc = r.intListesiAl(10);
+        listele(sonuc);
     }
 }
