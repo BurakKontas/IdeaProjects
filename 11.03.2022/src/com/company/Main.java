c7ad574b43b4de6e5e2abcc64af779a3dd57e358
BurakKontas <aburakkontas@trakya.edu.tr>
2022-03-14 15:23:45 +0300

    Overloading'e giriş

---
+++ 11.03.2022/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Random random = new Random();
+        var deger = random.nextInt();
+        System.out.println(deger);
+    }
+}
